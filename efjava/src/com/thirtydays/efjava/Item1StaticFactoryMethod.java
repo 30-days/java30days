73bf5ddf97919333cd89ceba4137dd1da165a21a
30-days <jerrygtd@yahoo.ca>
2013-08-28 23:46:20 -0400

    .

--- efjava/src/com/thirtydays/efjava/Item1StaticFactoryMethod.java
+++ efjava/src/com/thirtydays/efjava/Item1StaticFactoryMethod.java
@@ -14,10 +14,21 @@
  *			- item3: singleton
  *			- item4: noninstantiable
  *		* similar to FlyWeight design pattern
- *	3. Unlike constructors, they can return an object of any subtype
+ *	3. Unlike constructors, they can return an object of any sub-type
  *	   of their return type.
  *			- Hide the implementation of the classes
  *			- item18: interface based framework
+ *			- java.util.Collections for 32 implementations
+ *			- item32: java.util.EnumSet returns one of two implementations
+ *			- Basis of Service Provider Frameworks
+ *  4. Reduce the verbosity of creating parameterized type instances.
+ *  		- Map<String, List<String>> m = new HashMap<String, List<String>>();
+ *  		-> Map<String, List<String>> m = HashMap.newInstance();
+ *  		with the assumed method (not built in 1.6):
+ *  		public static <K, V> HashMap<K, V> newInstance() {
+ *  			return new HashMap<K, V>();
+ *  		}
+ *  		
  */
 
 package com.thirtydays.efjava;
